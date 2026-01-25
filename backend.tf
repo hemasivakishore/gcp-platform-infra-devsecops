741f78d12130bf04061275d532bb74797df21e04
hemasivakishore <hemasivakishore@gmail.com>
2026-01-25 21:01:01 +0530

    fix: tflint warnings for types and versions

--- backend.tf
+++ backend.tf
@@ -1,4 +1,5 @@
 terraform {
+  required_version = ">= 1.14.1"
   backend "gcs" {
     bucket = "gcp-platform-infra"
     prefix = "terraform/state"
--- variables.tf
+++ variables.tf
@@ -1,7 +1,9 @@
 variable "project_id" {
+  type = string
   default = "project-1e2da3fc-bb97-4b70-9c0"
 }
 
 variable "region" {
+  type = string
   default = "us-east1"
 }
