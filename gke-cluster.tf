84990dbe045474c92671e08967ac785f63579fde
hemasivakishore <hemasivakishore@gmail.com>
2026-01-26 19:02:11 +0530

    disk size and type chaged

--- gke-cluster.tf
+++ gke-cluster.tf
@@ -84,8 +84,8 @@ resource "google_container_node_pool" "primary_nodes" {
 
   node_config {
     machine_type = "e2-standard-4"
-    disk_size_gb = 10
-    disk_type    = "pd-balanced"
+    disk_size_gb = 20
+    disk_type    = "pd-standard"
 
     image_type = "COS_CONTAINERD"
 
