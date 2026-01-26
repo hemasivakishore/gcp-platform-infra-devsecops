056ff469305fbe8ec5ee398afa939c2088b6477c
hemasivakishore <hemasivakishore@gmail.com>
2026-01-26 22:10:04 +0530

    output variables added

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,72 @@
+#################################################
+# GKE Cluster Outputs
+#################################################
+
+output "cluster_name" {
+  description = "The name of the Kubernetes cluster."
+  value       = google_container_cluster.primary.name
+}
+
+output "cluster_endpoint" {
+  description = "The IP address of the cluster master."
+  value       = google_container_cluster.primary.endpoint
+  sensitive   = true # Marked sensitive because it's a private endpoint
+}
+
+output "cluster_location" {
+  description = "The region/zone where the cluster is deployed."
+  value       = google_container_cluster.primary.location
+}
+
+output "kubernetes_cluster_ca_certificate" {
+  description = "Public certificate of the cluster (base64 encoded)."
+  value       = google_container_cluster.primary.master_auth[0].cluster_ca_certificate
+  sensitive   = true
+}
+
+#################################################
+# Node Pool Outputs
+#################################################
+
+output "node_pool_name" {
+  description = "The name of the primary node pool."
+  value       = google_container_node_pool.primary_nodes.name
+}
+
+output "node_pool_status" {
+  description = "The current status of the node pool."
+  value       = google_container_node_pool.primary_nodes.operation
+}
+
+#################################################
+# Networking & Identity Outputs
+#################################################
+
+output "vpc_id" {
+  description = "The ID of the VPC."
+  value       = google_compute_network.vpc.id
+}
+
+output "nat_ips" {
+  description = "The public IP addresses used by the NAT gateway for egress."
+  value       = google_compute_address.router-ip[*].address
+}
+
 output "service_account_email" {
-  value = google_service_account.sa.email
+  description = "The email of the GKE service account."
+  value       = google_service_account.sa.email
+}
+
+output "workload_identity_pool" {
+  description = "The Workload Identity Pool for the cluster."
+  value       = google_container_cluster.primary.workload_identity_config[0].workload_pool
+}
+
+#################################################
+# Helper: kubectl connection command
+#################################################
+
+output "get_credentials_command" {
+  description = "Command to configure kubectl to connect to this cluster."
+  value       = "gcloud container clusters get-credentials ${google_container_cluster.primary.name} --zone ${google_container_cluster.primary.location} --project ${var.project_id}"
 }
