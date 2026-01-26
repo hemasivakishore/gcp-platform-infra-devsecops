17fb3b367f4407eaf94020b810f14f55b6707279
hemasivakishore <hemasivakishore@gmail.com>
2026-01-26 12:54:25 +0530

    typo change

--- route-nat.tf
+++ route-nat.tf
@@ -22,5 +22,5 @@ resource "google_compute_router_nat" "gke-nat" {
   region                             = google_compute_router.gke-router.region
   nat_ip_allocate_option             = "MANUAL_ONLY"
   nat_ips                            = google_compute_address.router-ip.*.self_link
-  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETS_ALL_IP_RANGES"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
 }
