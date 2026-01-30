790c7bb82eb6ce7208f786ac803336c00ec329d0
Hema Siva Kishore <70573794+hemasivakishore@users.noreply.github.com>
2026-01-30 16:59:29 +0530

    Update gke-cluster.tf

--- gke-cluster.tf
+++ gke-cluster.tf
@@ -173,7 +173,7 @@ resource "google_container_node_pool" "primary_nodes" {
 resource "google_logging_project_bucket_config" "gke_logs" {
   project        = var.project_id
   location       = "global"
-  bucket_id      = "gke-log-storage"
+  bucket_id      = "gke-log-monitor-storage"
   retention_days = 30
 }
 
